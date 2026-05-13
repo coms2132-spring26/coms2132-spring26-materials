f66ed9d4ba58724a7f19444a9276f18415867b1a
Daniel Bauer <bauer@cs.columbia.edu>
2026-05-12 22:12:04 -0400

    kbest solution

--- homework_solutions/homework3/kbest.py
+++ homework_solutions/homework3/kbest.py
@@ -1,20 +1,27 @@
 import heapq # recommended, but you can also use the heap implementation discussed in class
-
 import random # for testing
 
 class KBestCounter:
 
   def __init__(self,k):
     self.k = k
-    pass
+    self.heap = []
+
 
   def count(self, x): 
     """process the next element in the set of data."""
-    pass
+    if len(self.heap) < self.k:
+      heapq.heappush(self.heap, x)
+      return
+      
+    if x > self.heap[0]: # x should be part of the k-largest
+        heapq.heappop(self.heap)
+        heapq.heappush(self.heap, x)
+  
 
   def kbest(self): 
     """return a list of the $k$-largest elements that were ever passed to the `count` method."""
-    pass
+    return self.heap
 
 
 def test_k_best():
