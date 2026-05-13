8f035a844d81958ff500b63ea4792d37b4ea90bd
Daniel Bauer <bauer@cs.columbia.edu>
2026-05-12 22:11:54 -0400

    earthquakes solution

--- homework_solutions/homework3/earthquakes.py
+++ homework_solutions/homework3/earthquakes.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from datetime import datetime
+from kbest import KBestCounter
 
 class Earthquake:
 
@@ -16,6 +17,26 @@ class Earthquake:
     time = str(datetime.fromtimestamp(self.time)) 
     return f"{time} -- {self.mag} {self.place}"   
 
+  def __hash__(self): # for part a 
+    return hash(self.time) * 5 + \
+           hash(self.mag) * 7 + \
+           hash(self.place) * 11 + \
+           hash(self.longitude) * 13 + \
+           hash(self.latitude) * 17 + \
+           hash(self.depth) * 23
+  
+  def __eq__(self, other): # for part a
+    if not isinstance(other, Earthquake):
+      return False
+    return self.time == other.time \
+      and self.mag == other.mag \
+      and self.place == other.place \
+      and self.latitude == other.latitude \
+      and self.longitude == other.longitude \
+      and self.depth == other.depth
+
+  def __lt__(self, other): 
+    return self.mag < other.mag
 
 def fetch_earthquake_data():
     url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"
@@ -31,21 +52,52 @@ def fetch_earthquake_data():
 
 def print_data():
     while True:
-        earthquakes = fetch_earthquake_data()
-        for q in earthquakes: 
-          print(q)
-        print()
-        time.sleep(60)
+      earthquakes = fetch_earthquake_data()
+      for q in earthquakes: 
+        print(q)
+      print()
+      time.sleep(60)
 
 def print_new_quakes():
-    pass # Replace for part a) 
+  
+  quake_set = set()
+  while True: 
+    earthquakes = fetch_earthquake_data()
+    #quake_set.update(earthquakes) # only add new earthquakes 
+
+    # or step by step as follows
+    for q in earthquakes: 
+      if not q in quake_set: 
+        quake_set.add(q)
+        print(q)    
+    time.sleep(60)
+
 
 def print_k_largest(k=3):
-    pass # Replace for part b)
+
+  largest_quakes = KBestCounter(3) 
+  seen = set() 
+  while True: 
+    earthquakes = fetch_earthquake_data()
+    #quake_set.update(earthquakes) # only add new earthquakes 
+
+    # or step by step as follows
+    for q in earthquakes: 
+      if not q in seen: 
+        seen.add(q)
+        largest_quakes.count(q)
+
+    print(" ")
+    for q in largest_quakes.kbest():
+      print(q)
+    print("--- ") 
+    
+    time.sleep(60)
+
 
 if __name__ == "__main__":
 
     # only one of the following three functions shoudl be called at a time
-    print_data()
+    #print_data()
     #print_new_quakes() # for part a) 
-    #print_k_largest(3) # for part b)
+    print_k_largest(3) # for part b)
